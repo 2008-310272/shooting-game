4dd2fc2a30805282adbd17704bf7aed48d6130d0
2008-310272 <91247087+2008-310272@users.noreply.github.com>
2021-12-31 17:49:00 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,15 +15,24 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     主角.change(LedSpriteProperty.X, 1)
 })
+let 得分 = 0
 let 子彈: game.LedSprite = null
 let 主角: game.LedSprite = null
 game.setScore(0)
 主角 = game.createSprite(2, 4)
 let 飛機 = game.createSprite(0, 0)
 let 飛機2 = game.createSprite(1, 0)
+let 生命 = 3
 // 當飛機碰到就死了
 basic.forever(function () {
     if (飛機.isTouching(主角)) {
+        生命 += -1
+        game.pause()
+        basic.showString("life" + 生命)
+        飛機.set(LedSpriteProperty.X, 0)
+        飛機.change(LedSpriteProperty.Y, 1)
+        game.resume()
+    } else if (生命 == 0) {
         game.gameOver()
         basic.showString("score" + game.score())
     }
@@ -47,26 +56,14 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (飛機2.isTouching(主角)) {
-        game.gameOver()
-        basic.showString("score" + game.score())
-    }
+    得分 = 得分 + game.score()
 })
 basic.forever(function () {
-    basic.pause(randint(200, 500))
-    飛機2.change(LedSpriteProperty.X, 1)
-    if (飛機2.get(LedSpriteProperty.X) == 4) {
-        basic.pause(500)
-        飛機2.set(LedSpriteProperty.X, 0)
-        飛機2.change(LedSpriteProperty.Y, 1)
-    }
+	
 })
 basic.forever(function () {
-    if (子彈) {
-        if (子彈.isTouching(飛機2)) {
-            game.addScore(1)
-            飛機2.set(LedSpriteProperty.X, 0)
-            飛機2.set(LedSpriteProperty.Y, 0)
-        }
-    }
+	
+})
+basic.forever(function () {
+	
 })
